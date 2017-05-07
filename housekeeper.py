6bb8e8e16dea673f350766e1cc76b752bc00920f
Whatsapokemon <whatsapokemon@gmail.com>
2017-05-07 09:36:46 +0800

    Update on housekeeper.py
    Attempted to resolve long housekeeping times by doing all housekeeping in a single step, by downloading and uploading all cells with one gspread call each.
    
    This will either entirely succeed or entirely fail, and thus should resolve the half-broken housekeeping states we were getting.
    
    -Whatsa

--- housekeeper.py
+++ housekeeper.py
@@ -16,19 +16,34 @@ ServerSheet = ""
 
 
 def housekeeper():
-    curdate = datetime.date.today()
-    today = "{0}-{1}-{2}".format(curdate.month, curdate.day, curdate.year)
-    gc = gspread.authorize(credentials)
-    sheet_link = gc.open(ServerSheet).sheet1
-    for sheetname in sheet_link.col_values(1):
-        workingrow = sheet_link.col_values(1).index(sheetname)+1
-        if sheet_link.cell(workingrow,7).value == "yes":
-            newstreakscore = int(sheet_link.cell(workingrow,5).value)+1
-            sheet_link.update_cell(workingrow, 5, newstreakscore)
-        elif sheet_link.cell(workingrow,6).value == today:
-            sheet_link.update_cell(workingrow,5,0)
-        if sheet_link.cell(workingrow, 7).value != "Submitted Today?":
-            sheet_link.update_cell(workingrow, 7, "no")
+	curdate = datetime.date.today()
+	today = "{0}-{1}-{2}".format(curdate.month, curdate.day, curdate.year)
+	gc = gspread.authorize(credentials)
+	sheet_link = gc.open(ServerSheet).sheet1
+	#count rows
+	rowCount = sheet_link.row_count
+	print("Housekeeping on " + str(rowCount) + " rows on " + today)
+	#get all rows and put into memory
+	cell_list = sheet_link.range(1,1,rowCount, 7)
+	#update our saved version of the data
+	for cellIndex in range(1,rowCount):
+		# Update in batch
+		#if submitted is yes
+		rowIndex = cellIndex*7
+		if(cell_list[rowIndex+6].value == "yes"):
+			#update streak score by one
+			newstreakscore = int(cell_list[rowIndex+4].value)+1
+			cell_list[rowIndex+4].value = str(newstreakscore)
+		#else if streak ends today
+		elif(cell_list[rowIndex+5].value == today):
+			#set streak to 0
+			cell_list[rowIndex+4].value = "0"
+		#Set submitted to no
+		if(cell_list[rowIndex+6].value != "Submitted Today?"):
+			cell_list[rowIndex+6].value = "no"
+	#commit all changes to the sheet at once
+	sheet_link.update_cells(cell_list)
+	print("housekeeping finished")
 
 
 
