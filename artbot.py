cac9a81637fbafd888c5576ad1341bb6265e8184
Camero <fyrestyrm64@gmail.com>
2019-05-08 06:51:04 -0600

    quests has all code from master

--- artbot.py
+++ artbot.py
@@ -60,10 +60,14 @@ if(live):
     serverName = 'The Art Plaza Extravaganza'
     botChannelName = 'bot-channel'
     submitChannels = ['236678008562384896', '302915168801783810', '241060721994104833', '313945720447303680']
+    adoreEmoji = "284820985767788554"
+    adminRole = "Admins"
 else:
     serverName = 'Marsh Palace'
     botChannelName = 'bot-testing'
     submitChannels = ['271516310314156042','284618270659575818','386395766505340939','317414355501187072']
+    adoreEmoji = "538580881225285652"
+    adminRole = "Admins"
 ##########################
 
 
@@ -72,21 +76,21 @@ async def on_ready():
     global tapeServer
     global botChannel
     global adminRole
+    global adoreEmoji
     print('Bot Online.')
     print(client.user.name)
     print(client.user.id)
     print('------')
     #Store the server and the bot command channel
-    for s in client.servers:
-        if s.name == serverName:
-            tapeServer = s
-    for c in tapeServer.channels:
-        if c.name == botChannelName:
-            botChannel = c
+    tapeServer = discord.utils.find(lambda s: s.name == serverName, client.servers)
+    print("active server set to " + tapeServer.name)
+    botChannel = discord.utils.find(lambda c: c.name == botChannelName, tapeServer.channels)
+    print("bot channel set to " + botChannel.name)
     #admin role, admin actions require at least this role (or any with higher priority)
-    for r in tapeServer.roles:
-        if r.name == 'Admins':
-            adminRole = r
+    adminRole = discord.utils.find(lambda r: r.name == adminRole, tapeServer.roles)
+    print("admin role set to " + adminRole.name)
+    adoreEmoji = discord.utils.find(lambda e: e.id == adoreEmoji, tapeServer.emojis)
+    print("adore emoji set to " + adoreEmoji.name)
 
 
 @client.event
@@ -98,7 +102,7 @@ async def on_reaction_add(reaction, user):
         userToUpdate = reaction.message.mentions[0].id
     try:
         if type(reaction.emoji) is discord.Emoji:
-            if reaction.emoji.id == "284820985767788554" and user.id != userToUpdate and (reaction.message.content.startswith("!submit") or reaction.message.content.startswith("!proxysubmit")):
+            if reaction.emoji.id == adoreEmoji.id and user.id != userToUpdate and (reaction.message.content.startswith("!submit") or reaction.message.content.startswith("!proxysubmit")):
                 print("reaction added " + user.name + " " + str(reaction.emoji))
                 #find user in database using id
                 db_user = session.query(User).filter(User.id == userToUpdate).one()
@@ -118,7 +122,7 @@ async def on_reaction_remove(reaction, user):
         userToUpdate = reaction.message.mentions[0].id
     try:
         if type(reaction.emoji) is discord.Emoji:
-            if reaction.emoji.id == "284820985767788554" and user.id != userToUpdate and (reaction.message.content.startswith("!submit") or reaction.message.content.startswith("!proxysubmit")):
+            if reaction.emoji.id == adoreEmoji.id and user.id != userToUpdate and (reaction.message.content.startswith("!submit") or reaction.message.content.startswith("!proxysubmit")):
                 print("reaction removed " + user.name + " " + str(reaction.emoji))
                 #find user in database using id
                 db_user = session.query(User).filter(User.id == userToUpdate).one()
@@ -148,13 +152,14 @@ async def on_message(message):
                     pass
         else:
             await client.send_message(message.channel, "`Whoopsies, you can't submit in this channel!`")
-
     elif message.content.lower().startswith('!register') and message.author != message.author.server.me:
         curdate = datetime.utcnow()
         today = "{0}-{1}-{2}".format(curdate.month, curdate.day, curdate.year)
         already_registered = False
         #try to find user in database using id
         db_user = getDBUser(message.author.id)
+        serv = message.server
+        foundrole = discord.utils.find(lambda r: r.name == 'Artists', message.author.roles)
 
         #add a new user if there's no registered user
         if (db_user == None):
@@ -163,23 +168,41 @@ async def on_message(message):
             #add to session
             session.add(new_user)
             #give relevant roles
-            serv = message.server
             for rank in serv.roles:
                 if rank.name == "0+ Streak":
                     await client.add_roles(message.author, rank)
             for rank in serv.roles:
                 if rank.name == "Artists":
                     await client.add_roles(message.author, rank)
-
             #commit session
             session.commit()
             await client.send_message(message.channel, "```diff\n+ Successfully registered!\n```")
+        elif (db_user != None and foundrole == None):
+            for rank in serv.roles:
+                if rank.name == "Artists":
+                    await client.add_roles(message.author, rank)
+            await client.send_message(message.channel, "```Markdown\n# You're registered, I'll give you your Artist role back!\n```")
         else:
             await client.send_message(message.channel, "```Markdown\n# You're already registered!\n```")
 
     elif message.content.lower().startswith('!help') and message.author != message.author.server.me:
-        await client.send_message(message.channel,"```Markdown\n# Here's a quick little starter guide for all of you happy little artists wishing to participate.\n# !register will add you to our spreadsheet where we keep track of every submission you make\n# To submit content, drag and drop the file (.png, .gif, .jpg) into discord and add '!submit' as a comment to it.\n# If you'd like to submit via internet link, make sure you right click the image and select 'copy image location' and submit that URL using the !submit command.\n# The !timeleft command will let you know how much longer you have left to submit for the day!\n# To see your current scorecard, type !stats \n# To see your achievement status, type !ach\n# Having trouble figuring out what to draw? Override your role colour using !override <Role Number>\n# To turn on or off the PM warning system about your streak use the command !streakwarning on or !streakwarning off\n``` \n ```diff\n - For those of our older artists, you may access the nsfw channels by typing !nsfwjoin and you can hide those channels by typing !nsfwleave. \n - When submitting nsfwcontent please use the r18 channels respectively!!\n```")
-    elif message.content.lower().startswith('!stats') and message.author != message.author.server.me:
+        helpString = """```Markdown
+# Here's a quick little starter guide for all of you happy little artists wishing to participate.
+# !register will add you to our spreadsheet where we keep track of every submission you make
+# To submit content, drag and drop the file (.png, .gif, .jpg) into discord and add '!submit' as a comment to it.
+# If you'd like to submit via internet link, make sure you right click the image and select 'copy image location' and submit that URL using the !submit command.
+# The !timeleft command will let you know how much longer you have left to submit for the day!
+# To see your current scorecard, type !stats 
+# To see your achievement status, type !ach
+# Override your role colour using !override [Role Number], or !override none to clear an active override.
+# To turn on or off the PM warning system about your streak use the command !streakwarning on or !streakwarning off.
+```
+```diff
+- For those of our older artists, you may access the nsfw channels by typing !nsfwjoin and you can hide those channels by typing !nsfwleave. 
+- When submitting nsfwcontent please use the r18 channels respectively!!
+```"""
+        await client.send_message(message.channel, helpString)
+    elif message.content.lower().startswith('!stats') and message.channel == botChannel and message.author != message.author.server.me:
         #try to find user in database using id
         db_user = getDBUser(message.author.id)
 
@@ -891,29 +914,6 @@ async def on_message(message):
         await client.send_message(message.channel, "run housekeeping for testing\n")
         await housekeeper()
 
-    ##    elif message.content.lower().startswith('!quest') and message.author != message.author.server.me:
-    ##        filetypes = [".gif",".jpg",".jpeg",".png"]
-    ##        if ("https://" in message.content.lower() or "http://" in message.content.lower()) and any(u in message.content.lower() for u in filetypes) :
-    ##            # do linksubmit
-    ##            await linkSubmit(message, message.author)
-    ##        else:
-    ##            try:
-    ##                #normal submit.
-    ##                await normalSubmit(message, message.author)
-    ##            except:
-    ##                pass
-    ##
-    ##        db_user = getDBUser(message.author.id)
-    ##        questnumber = db_user.questdecay + 1
-    ##        db_user.questdecay = questnumber
-    ##        session.commit()
-
-    #log decrease
-    # elif message.content.lower().startswith('!log') and message.author != message.author.server.me:
-    #     db_user = getDBUser(message.author.id)
-    #     xp_gained = (1/(np.log(math.exp(db_user.questdecay)))) * 100
-    #     await client.send_message(message.channel, "the decay for quests is {0}\n".format(db_user.questdecay))
-
     elif message.content.lower().startswith('!questing') and message.author != message.author.server.me:
         #creates table for that user's quests
         for x in range(5):
@@ -943,6 +943,7 @@ async def on_message(message):
             session.commit()
         #commit session
         await client.send_message(message.channel, "nothing")
+
     elif message.content.lower().startswith('!board') and message.author != message.author.server.me:
 
         user = discord.utils.get(client.get_all_members(), id=str(message.author.id))
@@ -1108,6 +1109,8 @@ async def handleSubmit(message, userToUpdate, url):
                 session.commit()
                 print("finishing updating " + db_user.name + "'s stats")
                 await client.send_message(message.channel, "```diff\n+ @{0} Submission Successful! Score updated!\n+ {1}xp gained.```".format(userToUpdate.name,xp_gained))
+                #finally, add an adore to the submission
+                await client.add_reaction(message, adoreEmoji)
                 print("submit complete")
             else:
                 await client.send_message(message.channel, "```diff\n- Not a png, jpg, or gif file```")
